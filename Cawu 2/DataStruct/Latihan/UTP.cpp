671ca7233650b11cb7f8aa4dd38a85431a693d06
AdrielBernhardT <adrielbth01@gmail.com>
2025-02-21 14:29:50 +0700

    Update UTP.cpp for Exercise but not done yet

--- Cawu 2/DataStruct/Latihan/UTP.cpp
+++ Cawu 2/DataStruct/Latihan/UTP.cpp
@@ -2,16 +2,37 @@
 #include <stdlib.h>
 #include <system.h>
 #include <string.h>
+#include <time.h>
 
 struct Booking{
     char fullName[35];
     char phoneNum[15];
-    
+
 };
 
 
 
 int main(){
+    int choice;
+    do{
+        printf("Hotel GrAnsylvania\n");
+        printf("1. Booking Hotel\n");
+        printf("2. Print Bookings\n");
+        printf("3. Delete Booking\n");
+        printf("4. Exit\n");
+        scanf("%d", &choice); getchar();
+        if (choice < 1 || choice > 4){
+            printf("Invalid choice\n");
+        }
 
+        if(choice == 1){
+            bookingHotel();
+        }else if (choice == 2){
+            printBookings();
+        } else if (choice == 3){
+            deleteBooking();
+        }
+
+    }while (choice != 4)
     return 0;
 }
