e9684f45bd40e067570a0c25279c547e498ea896
AdrielBernhardT <adrielbth01@gmail.com>
2025-02-21 11:19:54 +0700

    Update .github workflow snake and updat HumanComputerInteraction Lab Code java and CSS

---
+++ Cawu 2/HumanComputerInteraction/Lab Code/script.js
@@ -0,0 +1,29 @@
+// let button = document.getElementById("button")
+
+// button.addEventListener("click", () => {
+//     for(let i = 0; i < 5; i++){
+//         alert("Button is clicked")
+//     }
+// })
+
+let square = document.getElementById("square")
+let step = 10
+
+document.addEventListener("keydown", (event) => {
+    let pos = square.getBoundingClientRect()
+    let top = pos.top
+    let left = pos.left
+
+    if(event.key == "ArrowUp"){
+        top -= step
+    } else if (event.key == "ArrowDown"){
+        top += step
+    } else if (event.key == "ArrowLeft"){
+        left -= step
+    } else if (event.key == "ArrowRight"){
+        left += step
+    }
+
+    square.style.top = top + "px"
+    square.style.left = left + "px"
+})
